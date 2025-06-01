e86f709dfa7e5053228dfdef743597294ff242e4
zenoe <2004lzy156@163.com>
2025-06-01 15:20:11 +0800

    prefix==>prefix-map

--- dap-config.el
+++ dap-config.el
@@ -4,37 +4,41 @@
 ;; download vscode-node-debug2
 ;; and unzip it (need unzip)
 (dap-node-setup)
+(after! dap-mode
+  (map! :map dap-mode-map
+        :leader
+        :prefix ("d" . "dap")
+        ;; basics
+        :desc "dap next"          "n" #'dap-next
+        :desc "dap step in"       "i" #'dap-step-in
+        :desc "dap step out"      "o" #'dap-step-out
+        :desc "dap continue"      "c" #'dap-continue
+        :desc "dap hydra"         "h" #'dap-hydra
+        :desc "dap debug restart" "r" #'dap-debug-restart
+        :desc "dap debug"         "s" #'dap-debug
 
-(map! :map dap-mode-map
-      :leader
-      :prefix ("d" . "dap")
-      ;; basics
-      :desc "dap next"          "n" #'dap-next
-      :desc "dap step in"       "i" #'dap-step-in
-      :desc "dap step out"      "o" #'dap-step-out
-      :desc "dap continue"      "c" #'dap-continue
-      :desc "dap hydra"         "h" #'dap-hydra
-      :desc "dap debug restart" "r" #'dap-debug-restart
-      :desc "dap debug"         "s" #'dap-debug
+        (:prefix-map ("d" . "Debug")
+                     (
+                      :desc "dap debug recent"  "r" #'dap-debug-recent
+                      :desc "dap debug last"    "l" #'dap-debug-last))
 
-      ;; debug
-      :prefix ("dd" . "Debug")
-      :desc "dap debug recent"  "r" #'dap-debug-recent
-      :desc "dap debug last"    "l" #'dap-debug-last
 
-      ;; eval
-      :prefix ("de" . "Eval")
-      :desc "eval"                "e" #'dap-eval
-      :desc "eval region"         "r" #'dap-eval-region
-      :desc "eval thing at point" "s" #'dap-eval-thing-at-point
-      :desc "add expression"      "a" #'dap-ui-expressions-add
-      :desc "remove expression"   "d" #'dap-ui-expressions-remove
+        ;; eval
+        (:prefix-map ("e" . "Eval")
+                     (:desc "eval"                "e" #'dap-eval
+                      :desc "eval region"         "r" #'dap-eval-region
+                      :desc "eval thing at point" "s" #'dap-eval-thing-at-point
+                      :desc "add expression"      "a" #'dap-ui-expressions-add
+                      :desc "remove expression"   "d" #'dap-ui-expressions-remove))
 
-      :prefix ("db" . "Breakpoint")
-      :desc "dap breakpoint toggle"      "b" #'dap-breakpoint-toggle
-      :desc "dap breakpoint condition"   "c" #'dap-breakpoint-condition
-      :desc "dap breakpoint hit count"   "h" #'dap-breakpoint-hit-condition
-      :desc "dap breakpoint log message" "l" #'dap-breakpoint-log-message)
+        ;; breakpoints
+        (:prefix-map ("b" . "Breakpoint")
+                     (:desc "dap breakpoint toggle"      "b" #'dap-breakpoint-toggle
+                      :desc "dap breakpoint condition"   "c" #'dap-breakpoint-condition
+                      :desc "dap breakpoint hit count"   "h" #'dap-breakpoint-hit-condition
+                      :desc "dap breakpoint log message" "l" #'dap-breakpoint-log-message))
+
+        ))
 
 (defun debug-with-dap ()
   (interactive)
